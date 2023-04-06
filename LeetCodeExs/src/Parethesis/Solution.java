682a370ed8887c2f8bb4482ef69207662e791648
Lucas Tizon Martins Dalmolin <78660963+ADalmolin3103@users.noreply.github.com>
2023-04-06 18:26:01 -0300

    2 exercises done.

---
+++ LeetCodeExs/src/Parethesis/Solution.java
@@ -0,0 +1,27 @@
+package Parethesis;
+
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case '(':
+                    stack.push(')');
+                    break;
+                case '[':
+                    stack.push(']');
+                    break;
+                case '{':
+                    stack.push('}');
+                    break;
+                default:
+                    if (stack.isEmpty() || c != stack.pop()) {
+                        return false;
+                    }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
---
+++ LeetCodeExs/src/TwoSum/Solution.java
@@ -0,0 +1,19 @@
+package TwoSum;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2]; 
+        for (int i = 0; i < nums.length; i++) {
+            
+            for ( int j = 0; j < nums.length; j++) {
+
+                if(i != j){
+                    if( nums[i]+nums[j] == target){
+                        return new int[]{i,j};
+                    }
+                }
+            }
+        }
+        return null;
+    }
+}
